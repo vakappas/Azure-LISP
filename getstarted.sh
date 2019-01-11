7f2051f6c5b2269b53e83e5edaf32c9f32bc653e
Jason Gmitter <jgmitter@cisco.com>
2019-01-11 17:27:12 -0500

    Update getstarted.sh

--- getstarted.sh
+++ getstarted.sh
@@ -2,7 +2,7 @@
 url=https://raw.githubusercontent.com/Microsoft/Azure-LISP/master/lisp_lab_iterate_through_vnet.json?token=ACh2r3wGMi9JHiYuCnCz9JjyG0I0Ejszks5cQeFzwA%3D%3D
 
 rg=lisplab
-az group create -n $rg -l useast
+az group create -n $rg -l eastus
 az group deployment create -g $rg --template-uri $url --parameters '{"adminPassword":{"value":"Msft123Msft123"}}'
 
 # Check Public IP addresses
