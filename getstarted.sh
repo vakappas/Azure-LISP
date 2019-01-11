71b631933a13faff6876ab3a84054e58d8a8aa57
Jason Gmitter <jgmitter@cisco.com>
2019-01-11 09:29:06 -0500

    Add files via upload

---
+++ getstarted.sh
@@ -0,0 +1,12 @@
+# Deploy lab
+url=https://raw.githubusercontent.com/Microsoft/Azure-LISP/master/lisp_lab_iterate_through_vnet.json?token=ACh2r3wGMi9JHiYuCnCz9JjyG0I0Ejszks5cQeFzwA%3D%3D
+
+rg=lisplab
+az group create -n $rg -l useast
+az group deployment create -g $rg --template-uri $url --parameters '{"adminPassword":{"value":"Msft123Msft123"}}'
+
+# Check Public IP addresses
+az network public-ip list -g $rg --query [].[name,ipAddress] -o tsv
+
+# Cleanup
+az group delete -n $rg -y --no-wait
