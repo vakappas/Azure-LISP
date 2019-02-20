4f96bb7715a042fabcba29538f180f4fa294c8b7
Vaggelis Kappas <vakappas@microsoft.com>
2019-02-20 06:52:59 +0200

    misc changes

--- getstarted.sh
+++ getstarted.sh
@@ -1,9 +1,9 @@
 # Deploy lab
-$url=https://raw.githubusercontent.com/Microsoft/Azure-LISP/master/lisp_lab_iterate_through_vnet.json
+url=https://raw.githubusercontent.com/Microsoft/Azure-LISP/master/lisp_lab_iterate_through_vnet.json
 
-$rg=lisplab
+rg=lisp-lab
 az group create -n $rg -l westeurope
-az group deployment create -g $rg --template-uri $url --parameters '{"adminPassword":{"value":"P@ssw0rd@2019"}}'
+az group deployment create -g $rg --template-file lisp_lab_iterate_through_vnet.json --parameters '{"adminPassword":{"value":"P@ssw0rd@2019"}}'
 
 # Check Public IP addresses
 az network public-ip list -g $rg --query [].[name,ipAddress] -o tsv
