6b8449a68ee3241f0794dd703fab345592680ee8
Vaggelis Kappas <vakappas@microsoft.com>
2019-02-20 06:51:53 +0200

    Changes to adapt the two subnet configuration

--- getstarted.sh
+++ getstarted.sh
@@ -1,9 +1,9 @@
 # Deploy lab
-url=https://raw.githubusercontent.com/Microsoft/Azure-LISP/master/lisp_lab_iterate_through_vnet.json
+$url=https://raw.githubusercontent.com/Microsoft/Azure-LISP/master/lisp_lab_iterate_through_vnet.json
 
-rg=lisplab
-az group create -n $rg -l eastus
-az group deployment create -g $rg --template-uri $url --parameters '{"adminPassword":{"value":"Msft123Msft123"}}'
+$rg=lisplab
+az group create -n $rg -l westeurope
+az group deployment create -g $rg --template-uri $url --parameters '{"adminPassword":{"value":"P@ssw0rd@2019"}}'
 
 # Check Public IP addresses
 az network public-ip list -g $rg --query [].[name,ipAddress] -o tsv
